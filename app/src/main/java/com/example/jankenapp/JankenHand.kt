6902231e0389f6b72afb5c960db0898a679d94b6
imakyo97 <kyoheishogun.1997@gmail.com>
2024-06-05 22:31:08 +0900

    R.stringからStringへの変換処理をenum classで定義

--- app/src/main/java/com/example/jankenapp/JankenHand.kt
+++ app/src/main/java/com/example/jankenapp/JankenHand.kt
@@ -1,5 +1,14 @@
 package com.example.jankenapp
 
-enum class JankenHand {
-    ROCK, SCISSORS, PAPER
+import android.content.Context
+import androidx.annotation.StringRes
+
+enum class JankenHand(@StringRes val resId: Int) {
+    ROCK(R.string.rock),
+    SCISSORS(R.string.scissors),
+    PAPER(R.string.paper);
+
+    fun toText(context: Context): String {
+        return context.getString(this.resId)
+    }
 }
--- app/src/main/java/com/example/jankenapp/JankenResult.kt
+++ app/src/main/java/com/example/jankenapp/JankenResult.kt
@@ -1,5 +1,14 @@
 package com.example.jankenapp
 
-enum class JankenResult {
-    WIN, LOSS, DRAW
+import android.content.Context
+import androidx.annotation.StringRes
+
+enum class JankenResult(@StringRes val resId: Int) {
+    WIN(R.string.your_win),
+    LOSS(R.string.your_losing),
+    DRAW(R.string.draw);
+
+    fun toText(context: Context): String {
+        return context.getString(this.resId)
+    }
 }
